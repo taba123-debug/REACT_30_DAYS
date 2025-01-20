cd1871a853f3634271cfc57e22dce193e1c86690
taba123-debug <tabbanghani123@gmail.com>
2025-01-20 20:15:08 +0500

    changes are added at day 5

---
+++ day_05/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Use_State from './components/Use_State'
+const App = () => {
+  return (
+    <div>
+     <Use_State/>
+    
+    </div>
+
+  )
+}
+
+export default App
---
+++ day_05/src/components/Use_State.js
@@ -0,0 +1,18 @@
+// steps to use useState
+// 1. import React from 'react'
+// 2. import { useState } from 'react'
+
+
+import React from 'react'
+import { useState } from 'react'
+const Use_State = () => {
+  return (
+    <div>
+      <button  style={{  padding: '10px', fontSize: '20px', backgroundColor: 'lightblue' }}>
+        Click Me
+      </button>
+    </div>
+  )
+}
+
+export default Use_State
